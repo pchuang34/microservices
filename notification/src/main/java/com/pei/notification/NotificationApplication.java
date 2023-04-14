77a7e7e40bdd28cde44a06dec208e9d24151a400
Pei Cheng Huang <peicheng@vt.edu>
2023-04-14 16:16:11 -0700

    set up properties for modules

--- notification/src/main/java/com/pei/notification/NotificationApplication.java
+++ notification/src/main/java/com/pei/notification/NotificationApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -18,6 +20,9 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients(
         basePackages = "com.pei.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
